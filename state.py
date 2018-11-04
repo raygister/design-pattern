f15e3fa633e9360084da2c77a7d49374b1dcc367
raylei <raygister@163.com>
2018-11-04 15:41:29 +0800

    modify state pattern

--- state.py
+++ state.py
@@ -12,112 +12,137 @@
 #       它们将会切换到什么样的状态（即任何状态改变都是定义好的）。
 
 
-class Node(object):
-    def __init__(self, tag_name, parent=None):
-        self.tag_name = tag_name
-        self.parent = parent
-        self.children = []
-        self.text = ''
+SOLD_OUT = 0
+NO_QUARTER = 1
+HAS_QUARTER = 2  # 被投入了25分钱
+SOLD = 3
 
-    def __str__(self):
-        if self.text:
-            return '{}: {}'.format(self.tag_name, self.text)
+
+# Context
+class GumballMachine(object):
+    def __init__(self, count):
+        self.state = SoldOutState(self)
+        self.count = count
+        if self.count > 0:
+            self.state = NoQuarterState(self)
+
+    # 将行为委托给状态
+    def insert_quarter(self):
+        self.state.insert_quarter()
+
+    def eject_quarter(self):
+        self.state.eject_quarter()
+
+    def turn_crank(self):
+        self.state.turn_crank()
+        self.state.dispense()
+    # end
+
+    def release_ball(self):
+        print('A gumball comes rolling out the slot...')
+        if self.count != 0:
+            self.count -= 1
+
+
+# State Interface
+class State(object):
+    def __init__(self, gumball_machine):
+        self.gumball_machine = gumball_machine
+
+    def insert_quarter(self):
+        pass
+
+    def eject_quarter(self):
+        pass
+
+    def turn_crank(self):
+        pass
+
+    def dispense(self):
+        pass
+
+
+# Concrete State
+class HasQuarterState(State):
+    def insert_quarter(self):
+        print('Please consume your quarter before you insert another quarter.')
+
+    def eject_quarter(self):
+        print('Quarter returned.')
+        self.gumball_machine.state = NoQuarterState(self.gumball_machine)
+
+    def turn_crank(self):
+        print('You turn the crank.')
+        self.gumball_machine.state = SoldState(self.gumball_machine)
+
+    def dispense(self):
+        print('Not at this state, please wait a second.')
+
+
+class SoldOutState(State):
+    def insert_quarter(self):
+        print('Please do not insert a quarter, the gumball is sold out.')
+
+    def eject_quarter(self):
+        print('You can not eject, you have not insert a quarter.')
+
+    def turn_crank(self):
+        print('You turn the crank, but there is no gumball.')
+
+    def dispense(self):
+        print('No gumball dispense.')
+
+
+class NoQuarterState(State):
+    def insert_quarter(self):
+        print('You insert a quarter.')
+        self.gumball_machine.state = HasQuarterState(self.gumball_machine)
+
+    def eject_quarter(self):
+        print('You can not eject, you have not insert a quarter.')
+
+    def turn_crank(self):
+        print('You turned the crank, there is no quarter.')
+
+    def dispense(self):
+        print('You need to pay first.')
+
+
+class SoldState(State):
+    def insert_quarter(self):
+        print('Please wait, we are giving you a gumball.')
+
+    def eject_quarter(self):
+        print('Sorry, you already turned the crank.')
+
+    def turn_crank(self):
+        print('Turning the crank twice do not get you another gumball!')
+
+    def dispense(self):
+        self.gumball_machine.release_ball()
+        if self.gumball_machine.count > 0:
+            self.gumball_machine.state = NoQuarterState(self.gumball_machine)
         else:
-            return self.tag_name
-
-
-class Parser(object):
-    def __init__(self, parse_str):
-        self.parse_str = parse_str
-        self.root = None
-        self.curr_node = None
-        self.state = first_tag
-
-    def process(self, remaining_str):
-        remaining = self.state.process(remaining_str, self)
-        if remaining:
-            self.process(remaining)
-
-    def start(self):
-        self.process(self.parse_str)
-
-
-class FirstTag(object):
-    @staticmethod
-    def process(remaining_str, parser):
-        index_start_tag, index_end_tag = remaining_str.find('<'), remaining_str.find('>')
-        tag_name = remaining_str[index_start_tag + 1: index_end_tag]
-        root = Node(tag_name)
-        parser.root = parser.curr_node = root
-        parser.state = child_node
-        return remaining_str[index_end_tag+1:]
-
-
-class ChildNode(object):
-    @staticmethod
-    def process(remaining_str, parser):
-        # 注意这里只做判断应该转移到哪个状态，并不处理解析
-        stripped = remaining_str.strip()
-        if stripped.startswith('</'):
-            parser.state = close_tag
-        elif stripped.startswith('<'):
-            parser.state = open_tag
-        else:
-            parser.state = text_node
-        return stripped
-
-
-class OpenTag(object):
-    @staticmethod
-    def process(remaining_str, parser):
-        index_start_tag, index_end_tag = remaining_str.find('<'), remaining_str.find('>')
-        tag_name = remaining_str[index_start_tag + 1: index_end_tag]
-        node = Node(tag_name, parser.curr_node)  # 以当前节点作为父节点创造新节点
-        parser.curr_node.children.append(node)  # 将新节点加到当前节点的子节点中
-        parser.curr_node = node  # 开始标签了，迭代到新节点处理
-        parser.state = child_node  # 状态转移
-        return remaining_str[index_end_tag + 1:]
-
-
-class CloseTag(object):
-    @staticmethod
-    def process(remaining_str, parser):
-        index_start_tag, index_end_tag = remaining_str.find('<'), remaining_str.find('>')
-        assert remaining_str[index_start_tag + 1] == '/'  # 确保为关闭标签
-        tag_name = remaining_str[index_start_tag + 2: index_end_tag]  # 取出关闭标签的属性值
-        assert tag_name == parser.curr_node.tag_name  # 确保关闭标签名与开始标签名相同
-        parser.curr_node = parser.curr_node.parent  # 关闭标签了，返回父节点处理
-        parser.state = child_node  # 状态转移
-        return remaining_str[index_end_tag + 1:].strip()
-
-
-class TextNode(object):
-    @staticmethod
-    def process(remaining_str, parser):
-        index_start_tag = remaining_str.find('<')
-        text = remaining_str[:index_start_tag]
-        parser.curr_node.text = text  # 把标签对应文本设置为当前节点的属性值
-        parser.state = child_node  # 状态转移
-        return remaining_str[index_start_tag:]
-
-
-# python的模块变量能够模仿单例，这里我们创建了可被重用的状态类实例
-first_tag = FirstTag()
-child_node = ChildNode()
-open_tag = OpenTag()
-close_tag = CloseTag()
-text_node = TextNode()
+            print('Oops, out of gumball.')
+            self.gumball_machine.state = SoldOutState(self.gumball_machine)
 
 
 if __name__ == '__main__':
-    with open('./state.xml') as file:
-        contents = file.read()
-        parser = Parser(contents)
-        parser.start()
-
-        # BFS
-        nodes = [parser.root]
-        while nodes:
-            node = nodes.pop(0)
-            print(node)
-            nodes = node.children + nodes
+    gumballMachine = GumballMachine(2)
+    print(gumballMachine.count)
+    print(gumballMachine.state)
+    print("=====================================================")
+    gumballMachine.insert_quarter()
+    print(gumballMachine.state)
+    gumballMachine.eject_quarter()
+    gumballMachine.eject_quarter()
+    gumballMachine.insert_quarter()
+    print(gumballMachine.state)
+    gumballMachine.turn_crank()
+    print(gumballMachine.state)
+    print(gumballMachine.count)
+    gumballMachine.insert_quarter()
+    gumballMachine.turn_crank()
+    print(gumballMachine.state)
+    print("=====================================================")
+    gumballMachine.turn_crank()
